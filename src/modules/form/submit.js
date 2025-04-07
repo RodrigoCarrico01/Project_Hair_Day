9a15c2d14bae6534a4716f1ee2cc5c0459a4ebd9
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-04-07 17:01:15 +0100

    object that stores the form items

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 
 const form = document.querySelector('form');
+const clientName = document.getElementById('client')
 const selectedDate = document.getElementById('date')
 
 //Data atual para o formatar o input
@@ -15,5 +16,38 @@ form.onsubmit = (event) => {
   //Previne o comportamento padrão de recarregar a página
   event.preventDefault()
 
-  console.log("Enviado!")
+  try{
+    //Recuperar o nome do cliente
+    const name = clientName.value.trim()
+
+    if(!name){
+      return alert("Preencha o nome do cliente.")
+    }
+
+    //Recupera o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected")
+
+    if(!hourSelected){
+      return alert("Selecione um horário.")
+    }
+
+    //Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(":")
+
+    //Insere a hora na data.
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    //cria um id
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name,
+      when: when.format(),
+      hour: when.format("HH:mm"),
+    })
+  }catch(error){
+    alert("Não foi possível realizar o agendamento.")
+    console.log(error)
+  }
 } 
