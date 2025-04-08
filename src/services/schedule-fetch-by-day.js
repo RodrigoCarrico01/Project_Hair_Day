81d5e6c3f169e721f36cb218d0a860a4ee7f961a
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-04-08 09:30:33 +0100

    fix day

--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -10,7 +10,7 @@ export async function scheduleFetchByDay({date}){
     const data = await response.json()
 
     //Filtra os agendamentos pelo dia selecionado
-    const dailySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, "date"))
+    const dailySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, "day"))
 
     return dailySchedules
   } catch (error){
