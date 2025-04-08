d41894f0ccbace5f75ff1b27d8a49d197f2798d1
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-04-08 09:59:38 +0100

    appointments appearing in the interface

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,10 +1,18 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+import {schedulesShow} from "../schedules/show.js"
 import {hoursLoad} from "../form/hours-load.js"
 
 //Seleciona o input de data.
 const selectedDate = document.getElementById('date')
-export function schedulesDay(){
+export async function schedulesDay(){
   //Obtem a data do input
   const date = selectedDate.value
+
+  //Buscar na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date })
+
+  // Exibe os agendamentos
+  schedulesShow({dailySchedules})
 
   //Renderiza as horas disponíveis
   hoursLoad({date})
