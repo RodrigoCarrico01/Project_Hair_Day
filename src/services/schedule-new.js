9ffe121d7bd32c50157faa8e7be025bf9f5a359d
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-04-23 14:48:27 +0100

    Cancel working again

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,14 +2,13 @@ import { apiConfig } from "./api-config.js"
 
 export async function scheduleNew({id, name, when}){
   try {
-    id.toString()
     //Fazer a requesição para enviar os dados do agendamento
     await fetch(`${apiConfig.baseURL}/schedules`, {
       method: 'POST',
       headers:{
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({id, name, when})
+      body: JSON.stringify({id: String(id), name, when})
     })
 
     //Exibir uma mensagem de sucesso
